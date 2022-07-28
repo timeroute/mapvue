51ef180abee442cbc1d7f6478eb174242deca0d7
timeroute <chinesejar@gmail.com>
2022-07-28 22:52:15 +0800

    Update Fog props

--- packages/Common/Fog.vue
+++ packages/Common/Fog.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import type { Expression } from "mapbox-gl";
 import { inject, onMounted, onUnmounted, watch } from "vue";
+import type { Expression } from "mapbox-gl";
 import { mapvueSymbol } from "../symbols";
 
 interface Props {
   color?: string | Expression | undefined;
+  "high-color"?: string | Expression | undefined;
+  "space-color"?: string | Expression | undefined;
   "horizon-blend"?: number | Expression | undefined;
   range?: number[] | Expression | undefined;
+  "star-intensity"?: number | Expression | undefined;
 }
 
 const map = inject(mapvueSymbol);
@@ -28,12 +31,27 @@ watch(
 );
 
 watch(
+  () => props["high-color"],
+  () => renderFog(props)
+);
+
+watch(
+  () => props["space-color"],
+  () => renderFog(props)
+);
+
+watch(
+  () => props["star-intensity"],
+  () => renderFog(props)
+);
+
+watch(
   () => props.range,
   () => renderFog(props)
 );
 
 onMounted(() => {
-  renderFog();
+  renderFog(props || {});
 });
 
 onUnmounted(() => {
