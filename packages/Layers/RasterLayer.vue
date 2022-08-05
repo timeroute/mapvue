0d9387f94c46b5cb3e6d8c06872b7635f1df0e35
timeroute <chinesejar@gmail.com>
2022-08-05 18:12:20 +0800

    Fixed mouse event in layers

--- packages/Layers/RasterLayer.vue
+++ packages/Layers/RasterLayer.vue
@@ -31,6 +31,21 @@ const emits = defineEmits<{
   (e: "mouseleave", event: EventData): void;
 }>();
 
+if (map) {
+  useLayerEvent(map.value, "click", props.id, (e) => {
+    emits("click", e);
+  });
+  useLayerEvent(map.value, "mouseenter", props.id, (e) => {
+    emits("mouseenter", e);
+  });
+  useLayerEvent(map.value, "mousemove", props.id, (e) => {
+    emits("mousemove", e);
+  });
+  useLayerEvent(map.value, "mouseleave", props.id, (e) => {
+    emits("mouseleave", e);
+  });
+}
+
 const updatePaintProperty = (name: string, value: unknown) => {
   if (!map || !layer.value) return;
   map.value.setPaintProperty(props.id, name, value);
@@ -97,19 +112,6 @@ onMounted(() => {
     options.filter = props.filter;
   }
   map.value.addLayer(options);
-
-  useLayerEvent(map.value, "click", props.id, (e) => {
-    emits("click", e);
-  });
-  useLayerEvent(map.value, "mouseenter", props.id, (e) => {
-    emits("mouseenter", e);
-  });
-  useLayerEvent(map.value, "mousemove", props.id, (e) => {
-    emits("mousemove", e);
-  });
-  useLayerEvent(map.value, "mouseleave", props.id, (e) => {
-    emits("mouseleave", e);
-  });
 
   layer.value = map.value.getLayer(props.id);
 });
